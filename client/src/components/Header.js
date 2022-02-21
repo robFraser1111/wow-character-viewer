a9c7e8fe417b7f5e49530249886535caf1af2f63
robFraser1111 <robfraserdesign@gmail.com>
2022-02-21 21:43:59 +1100

    Updated URLs

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,116 +4,113 @@ import styled from "styled-components";
 import WowLogo from "../images/wow-logo-01.png";
 
 const Wrapper = styled.header`
-    padding: 20px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: ${(props) => props.theme.white};
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${(props) => props.theme.white};
 `;
 
 const Items = styled.section`
-    max-width: ${(props) => props.theme.laptop};
-    display: flex;
-    align-items: center;
-    width: 100%;
-    justify-content: space-between;
+  max-width: ${(props) => props.theme.laptop};
+  display: flex;
+  align-items: center;
+  width: 100%;
+  justify-content: space-between;
 
-    h1 {
-        margin-bottom: 8px;
-    }
+  h1 {
+    margin-bottom: 8px;
+  }
 
-    p {
-        margin-top: 8px;
-        margin-left: 40px;
-        font-weight: 600;
-    }
+  p {
+    margin-top: 8px;
+    margin-left: 40px;
+    font-weight: 600;
+  }
 `;
 
 const Left = styled.div`
-    margin-right: 40px;
+  margin-right: 40px;
 `;
 
 const Right = styled.div`
-    margin-bottom: 40px;
+  margin-bottom: 40px;
 `;
 
 const Item = styled.section`
-    max-width: ${(props) => props.theme.laptop};
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
-    justify-content: center;
+  max-width: ${(props) => props.theme.laptop};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  justify-content: center;
 `;
 
 const Logout = styled.a`
-    color: ${(props) => props.theme.white};
-    border: 1px solid ${(props) => props.theme.white};
-    padding: 10px 20px;
+  color: ${(props) => props.theme.white};
+  border: 1px solid ${(props) => props.theme.white};
+  padding: 10px 20px;
 
-    &:hover,
-    &:focus {
-        color: ${(props) => props.theme.bgColor};
-        background-color: ${(props) => props.theme.white};
-    }
+  &:hover,
+  &:focus {
+    color: ${(props) => props.theme.bgColor};
+    background-color: ${(props) => props.theme.white};
+  }
 `;
 
 const Logo = styled.img`
-    width: 100%;
-    transition: ${(props) => props.theme.transition};
+  width: 100%;
+  transition: ${(props) => props.theme.transition};
 
-    &:hover, &: focus {
-        opacity: 0.8;
-    }
+  &:hover, &: focus {
+    opacity: 0.8;
+  }
 `;
 
 const Header = (props) => {
-    const user = useContext(UserContext);
+  const user = useContext(UserContext);
 
-    if (user?.token === "") {
-        return (
-            <Wrapper>
-                <Item>
-                    <div>
-                        <a href={`${process.env.REACT_APP_DOMAIN}:3000`}>
-                            <Logo src={WowLogo} alt="WoW Logo" />
-                        </a>
-                    </div>
-                </Item>
-            </Wrapper>
-        );
-    } else {
-        return (
-            <Wrapper>
-                <Items>
-                    <Left>
-                        <a href={`${process.env.REACT_APP_DOMAIN}:3000`}>
-                            <Logo src={WowLogo} alt="WoW Logo" />
-                        </a>
-                        <p>
-                            {props?.currentPlayer?.battletag ? (
-                                <>
-                                    <small>
-                                        BattleTag:{" "}
-                                        <b>{props?.currentPlayer?.battletag}</b>
-                                    </small>
-                                </>
-                            ) : (
-                                ""
-                            )}
-                        </p>
-                    </Left>
-                    <Right>
-                        <Logout
-                            href={`${process.env.REACT_APP_DOMAIN}:5000/logout`}
-                        >
-                            Logout
-                        </Logout>
-                    </Right>
-                </Items>
-            </Wrapper>
-        );
-    }
+  if (user?.token === "") {
+    return (
+      <Wrapper>
+        <Item>
+          <div>
+            <a href={`https://wow-character-app.herokuapp.com`}>
+              <Logo src={WowLogo} alt="WoW Logo" />
+            </a>
+          </div>
+        </Item>
+      </Wrapper>
+    );
+  } else {
+    return (
+      <Wrapper>
+        <Items>
+          <Left>
+            <a href={`https://wow-character-app.herokuapp.com`}>
+              <Logo src={WowLogo} alt="WoW Logo" />
+            </a>
+            <p>
+              {props?.currentPlayer?.battletag ? (
+                <>
+                  <small>
+                    BattleTag: <b>{props?.currentPlayer?.battletag}</b>
+                  </small>
+                </>
+              ) : (
+                ""
+              )}
+            </p>
+          </Left>
+          <Right>
+            <Logout href={`https://wow-character-app.herokuapp.com/logout`}>
+              Logout
+            </Logout>
+          </Right>
+        </Items>
+      </Wrapper>
+    );
+  }
 };
 
 export default Header;
--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -4,146 +4,146 @@ import SpinnerImage from "../images/spinner-battlenet-01.png";
 import styled from "styled-components";
 
 const Form = styled.form`
-    color: ${(props) => props.theme.white};
+  color: ${(props) => props.theme.white};
 `;
 
 const Select = styled.div`
-    margin: 20px 0;
+  margin: 20px 0;
 
-    label,
-    select {
-        display: block;
-    }
+  label,
+  select {
+    display: block;
+  }
 
-    label {
-        margin-bottom: 5px;
-    }
+  label {
+    margin-bottom: 5px;
+  }
 
-    select {
-        width: 100%;
-        padding: 10px 5px;
-        color: ${(props) => props.theme.white};
-        background-color: ${(props) => props.theme.bgColor};
-        border: 1px solid ${(props) => props.theme.white};
-    }
+  select {
+    width: 100%;
+    padding: 10px 5px;
+    color: ${(props) => props.theme.white};
+    background-color: ${(props) => props.theme.bgColor};
+    border: 1px solid ${(props) => props.theme.white};
+  }
 `;
 
 const Button = styled.button`
-    background-color: ${(props) => props.theme.blue};
-    border: 1px solid ${(props) => props.theme.blueLight};
-    width: 360px;
-    height: auto;
-    padding: 10px 0;
-    color: ${(props) => props.theme.white};
-    cursor: pointer;
+  background-color: ${(props) => props.theme.blue};
+  border: 1px solid ${(props) => props.theme.blueLight};
+  width: 360px;
+  height: auto;
+  padding: 10px 0;
+  color: ${(props) => props.theme.white};
+  cursor: pointer;
 
-    &:hover,
-    &:focus {
-        background-color: ${(props) => props.theme.blueLight};
-    }
+  &:hover,
+  &:focus {
+    background-color: ${(props) => props.theme.blueLight};
+  }
 
-    &:active {
-        background-color: ${(props) => props.theme.blueDark};
-    }
+  &:active {
+    background-color: ${(props) => props.theme.blueDark};
+  }
 
-    &.loading-true {
-        background-color: ${(props) => props.theme.blueDark};
-        b {
-            display: none;
-        }
-        i {
-            display: inline-block;
-        }
+  &.loading-true {
+    background-color: ${(props) => props.theme.blueDark};
+    b {
+      display: none;
     }
+    i {
+      display: inline-block;
+    }
+  }
 `;
 
 const Spinner = styled.i`
-    width: 20px;
-    height: 20px;
-    background-image: url(${SpinnerImage});
-    background-position: -140px 0;
+  width: 20px;
+  height: 20px;
+  background-image: url(${SpinnerImage});
+  background-position: -140px 0;
 
-    display: none;
+  display: none;
 
-    animation: keyframes-loading 0.8s steps(21) infinite;
+  animation: keyframes-loading 0.8s steps(21) infinite;
 
-    @keyframes keyframes-loading {
-        0% {
-            background-position: 0;
-        }
-        100% {
-            background-position: -420px;
-        }
+  @keyframes keyframes-loading {
+    0% {
+      background-position: 0;
     }
+    100% {
+      background-position: -420px;
+    }
+  }
 `;
 
 const LoginForm = (props) => {
-    const user = useContext(UserContext);
-    const [loading, setLoading] = useState(false);
+  const user = useContext(UserContext);
+  const [loading, setLoading] = useState(false);
 
-    const handleLoading = () => {
-        setLoading(true);
-    };
+  const handleLoading = () => {
+    setLoading(true);
+  };
 
-    return (
-        <Form action={`${process.env.REACT_APP_DOMAIN}:5000/oauth/battlenet`}>
-            <Select>
-                <label htmlFor="region">
-                    <h5>Region: </h5>
-                </label>
-                <select
-                    name="region"
-                    id="region"
-                    value={user.region || ''}
-                    onChange={props.regionHandler}
-                    required
-                >
-                    <option value="us">North America</option>
-                    <option value="eu">Europe</option>
-                    <option value="kr">Korea</option>
-                    <option value="tw">Taiwan</option>
-                    <option value="cn">China</option>
-                </select>
-            </Select>
+  return (
+    <Form action={`https://wow-character-app.herokuapp.com/oauth/battlenet`}>
+      <Select>
+        <label htmlFor="region">
+          <h5>Region: </h5>
+        </label>
+        <select
+          name="region"
+          id="region"
+          value={user.region || ""}
+          onChange={props.regionHandler}
+          required
+        >
+          <option value="us">North America</option>
+          <option value="eu">Europe</option>
+          <option value="kr">Korea</option>
+          <option value="tw">Taiwan</option>
+          <option value="cn">China</option>
+        </select>
+      </Select>
 
-            <Select>
-                <label htmlFor="locale">
-                    <h5>Language: </h5>
-                </label>
-                <select
-                    name="locale"
-                    id="locale"
-                    value={user.locale || ''}
-                    onChange={props.localeHandler}
-                    required
-                >
-                    <option value="en_US">English (US)</option>
-                    <option value="es_MX">Mexican Spanish</option>
-                    <option value="pt_BR">Portuguese</option>
-                    <option value="en_GB">English (GB)</option>
-                    <option value="es_ES">European Spanish</option>
-                    <option value="fr_FR">French</option>
-                    <option value="ru_RU">Russian</option>
-                    <option value="de_DE">German</option>
-                    <option value="pt_PT">Portuguese</option>
-                    <option value="it_IT">Italian</option>
-                    <option value="ko_KR">Korean</option>
-                    <option value="zh_TW">Mandarin</option>
-                    <option value="zh_CN">Chinese</option>
-                </select>
-            </Select>
+      <Select>
+        <label htmlFor="locale">
+          <h5>Language: </h5>
+        </label>
+        <select
+          name="locale"
+          id="locale"
+          value={user.locale || ""}
+          onChange={props.localeHandler}
+          required
+        >
+          <option value="en_US">English (US)</option>
+          <option value="es_MX">Mexican Spanish</option>
+          <option value="pt_BR">Portuguese</option>
+          <option value="en_GB">English (GB)</option>
+          <option value="es_ES">European Spanish</option>
+          <option value="fr_FR">French</option>
+          <option value="ru_RU">Russian</option>
+          <option value="de_DE">German</option>
+          <option value="pt_PT">Portuguese</option>
+          <option value="it_IT">Italian</option>
+          <option value="ko_KR">Korean</option>
+          <option value="zh_TW">Mandarin</option>
+          <option value="zh_CN">Chinese</option>
+        </select>
+      </Select>
 
-            <Button
-                // onClick={handleLoading}
-                type="submit"
-                value="Submit"
-                className={"loading-" + loading.toString()}
-            >
-                <h5>Login</h5>
-                <Spinner></Spinner>
-            </Button>
-        </Form>
-    );
+      <Button
+        // onClick={handleLoading}
+        type="submit"
+        value="Submit"
+        className={"loading-" + loading.toString()}
+      >
+        <h5>Login</h5>
+        <Spinner></Spinner>
+      </Button>
+    </Form>
+  );
 };
 
 export default LoginForm;
