eaf266d67b2f0482bc49be4a3c8edd3341b377a5
robFraser1111 <robfraserdesign@gmail.com>
2022-02-21 19:41:27 +1100

    Updated for Heroku

--- server.js
+++ server.js
@@ -7,58 +7,59 @@ const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const BnetStrategy = require("passport-bnet").Strategy;
 
+const PORT = process.env.PORT || 5000;
 const BNET_ID = process.env.BNET_OAUTH_CLIENT_ID;
 const BNET_SECRET = process.env.BNET_OAUTH_CLIENT_SECRET;
 const DOMAIN = process.env.DOMAIN;
 const OAUTH_CALLBACK_URL =
-    process.env.OAUTH_CALLBACK_URL || `${DOMAIN}:5000/oauth/battlenet/callback`;
+  process.env.OAUTH_CALLBACK_URL || `${DOMAIN}:${PORT || 5000}/oauth/battlenet/callback`;
 // Review full list of available scopes here: https://develop.battle.net/documentation/guides/using-oauth
 const OAUTH_SCOPES = process.env.OAUTH_SCOPES || "wow.profile";
 
 passport.serializeUser(function (user, done) {
-    done(null, user);
+  done(null, user);
 });
 
 passport.deserializeUser(function (obj, done) {
-    done(null, obj);
+  done(null, obj);
 });
 
 // Register the BnetStrategy within Passport.
 passport.use(
-    new BnetStrategy(
-        {
-            clientID: BNET_ID,
-            clientSecret: BNET_SECRET,
-            scope: OAUTH_SCOPES,
-            callbackURL: OAUTH_CALLBACK_URL,
-        },
-        function (accessToken, refreshToken, profile, done) {
-            process.nextTick(function () {
-                return done(null, profile);
-            });
-        }
-    )
+  new BnetStrategy(
+    {
+      clientID: BNET_ID,
+      clientSecret: BNET_SECRET,
+      scope: OAUTH_SCOPES,
+      callbackURL: OAUTH_CALLBACK_URL,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      process.nextTick(function () {
+        return done(null, profile);
+      });
+    }
+  )
 );
 
 const app = express();
 
 // set up cors to allow us to accept requests from our client
 app.use(
-    cors({
-        origin: `${DOMAIN}:3000`, // allow to server to accept request from different origin
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-        credentials: true, // allow session cookie from browser to pass through
-    })
+  cors({
+    origin: `${DOMAIN}:3000`, // allow to server to accept request from different origin
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // allow session cookie from browser to pass through
+  })
 );
 
 // configure Express
 app.use(cookieParser());
 app.use(
-    session({
-        secret: "passport-battlenet-example", // Change this value to a unique value for your application!
-        saveUninitialized: true,
-        resave: true,
-    })
+  session({
+    secret: "passport-battlenet-example", // Change this value to a unique value for your application!
+    saveUninitialized: true,
+    resave: true,
+  })
 );
 
 // Initialize Passport! Also use passport.session() middleware, to support
@@ -69,33 +70,33 @@ app.use(passport.session());
 app.get("/oauth/battlenet", passport.authenticate("bnet"));
 
 app.get(
-    "/oauth/battlenet/callback",
-    passport.authenticate("bnet", {
-        failureRedirect: `${DOMAIN}:3000/`,
-    }),
-    function (req, res) {
-        res.redirect(`${DOMAIN}:3000/`);
-    }
+  "/oauth/battlenet/callback",
+  passport.authenticate("bnet", {
+    failureRedirect: `${DOMAIN}:3000/`,
+  }),
+  function (req, res) {
+    res.redirect(`${DOMAIN}:3000/`);
+  }
 );
 
 app.get("/api", function (req, res) {
-    if (req.isAuthenticated()) {
-        res.json(req.user);
-    } else {
-        res.json("Logged out");
-    }
+  if (req.isAuthenticated()) {
+    res.json(req.user);
+  } else {
+    res.json("Logged out");
+  }
 });
 
 app.get("/logout", function (req, res) {
-    req.logout();
-    res.redirect(`${DOMAIN}:3000/`);
+  req.logout();
+  res.redirect(`${DOMAIN}:3000/`);
 });
 
 app.use(function (err, req, res, next) {
-    console.error(err);
-    res.send("<h1>Internal Server Error</h1>");
+  console.error(err);
+  res.send("<h1>Internal Server Error</h1>");
 });
 
-const server = app.listen(5000, function () {
-    console.log("Listening on port %d boyo", server.address().port);
+const server = app.listen(PORT || 5000, function () {
+  console.log("Listening on port %d boyo", server.address().port);
 });
