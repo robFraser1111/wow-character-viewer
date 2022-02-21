ae1f906075cadadf12ed3e0366b879f64ec1f141
robFraser1111 <robfraserdesign@gmail.com>
2022-02-21 20:40:16 +1100

    Updated server and packages

--- server.js
+++ server.js
@@ -102,31 +102,17 @@ const server = app.listen(PORT || 5000, function () {
   console.log("Listening on port %d boyo", server.address().port);
 });
 
-// ** MIDDLEWARE ** //
-const whitelist = [
-  "http://localhost:3000",
-  "http://localhost:5000",
-  "https://wow-character-app.herokuapp.com/",
-];
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin);
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable");
-      callback(null, true);
-    } else {
-      console.log("Origin rejected");
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+// ---------------- ADD THIS ----------------
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+// --------------------------------
 
-// ** SERVE CLIENT AS HOME PAGE ** //
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
-  // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
+app.use("/", indexRouter);
+
+// ---------------- ADD THIS ----------------
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+// --------------------------------
